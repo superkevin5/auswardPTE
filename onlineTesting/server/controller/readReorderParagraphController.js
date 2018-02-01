2f40697b64f3cab8ecb33cbeafd7aa4111554c01
kevinHu <kevin.hu@macquarie.com>
2018-02-01 18:04:05 +1100

    add angular dragable

--- onlineTesting/server/controller/readReorderParagraphController.js
+++ onlineTesting/server/controller/readReorderParagraphController.js
@@ -4,16 +4,43 @@
 var readReorderParagraph = require('../model/readReorderParagraph');
 var pteConstants = require('../utility/constant.js');
 
-exports.getAllReadReorderParagraphIds = function(req, res) {
+exports.getAllReadReorderParagraphIds = function (req, res) {
 
-    req.models.readReorderParagraph.all(function(error,data){
+    req.models.readReorderParagraph.all(function (error, data) {
+
+        if (error) {
+            res.status(pteConstants.InternalServerError);
+        }
+        var ids = [];
+        if (data) {
+            for (var i = 0; i < data.length; i++) {
+                ids.push(data[i].id);
+            }
+        }
+        res.status(200).json(ids);
+    });
+
+};
+
+exports.getReadingFillBlankById = function (req, res) {
+
+
+    req.models.readReorderParagraph.get(req.params.id,function (error, data) {
 
         if (error) {
             res.status(pteConstants.InternalServerError);
         }
 
-        return res.status(200).json(data);
+
+        req.models.readReorderParagraph.find({questionfk:req.params.id},function(err,result){
+
+
+            
+            res.status(200).json(data);
+        });
+
+
+
     });
 
 };
-
