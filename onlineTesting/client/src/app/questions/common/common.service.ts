9f6da26688cc5d065a43d05c50a31bb802106c17
kevinHu <kevin.hu@macquarie.com>
2018-02-01 15:19:05 +1100

    Fix array type

--- onlineTesting/client/src/app/questions/common/common.service.ts
+++ onlineTesting/client/src/app/questions/common/common.service.ts
@@ -12,7 +12,7 @@ export class CommonService implements OnInit {
 
   }
 
-  shuffleArray(array:Array){
+  shuffleArray(array:Array<any>){
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
