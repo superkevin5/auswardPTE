9a16d5006a9705d71e292222bebde703f94a7f57
kevinHu <kevin.hu@macquarie.com>
2018-03-01 18:35:43 +1100

    Finish answer short question

--- onlineTesting/server/app.js
+++ onlineTesting/server/app.js
@@ -38,7 +38,7 @@ var logStdout = process.stdout;
 
 var limiter = new RateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 200, // limit each IP to 100 requests per windowMs
     delayMs: 0, // disable delaying - full speed until the max limit is reached,
     message: 'too many requests, you are blocked!!'
 });
