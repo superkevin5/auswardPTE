69912df016c8e1c6385227d9244e8b583136d0e8
serkevin5 <hlmloveaustralia@gmail.com>
2018-03-05 20:35:27 +1100

    Fix issue in IE

--- onlineTesting/client/src/app/questions/writeEssay/writeEssay.component.ts
+++ onlineTesting/client/src/app/questions/writeEssay/writeEssay.component.ts
@@ -80,17 +80,6 @@ export class WriteEssayComponent implements OnInit,AfterContentInit,OnDestroy {
     if (window.navigator && window.navigator.msSaveOrOpenBlob) {
       window.navigator.msSaveOrOpenBlob(blob, filename);
     }
-    else {
-      var e = document.createEvent('MouseEvents'),
-        a = document.createElement('a');
-
-      a.download = filename;
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
-      e.initEvent('click', true, false, window,
-        0, 0, 0, 0, 0, false, false, false, false, 0, null);
-      a.dispatchEvent(e);
-    }
   }
 
 
